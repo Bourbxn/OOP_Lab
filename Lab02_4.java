5f71c27d3ea6cc08c4d731497c7e23e1a375d077
Jakkapat Paijit <86193685+Bourbxn@users.noreply.github.com>
2022-01-20 14:52:17 +0700

    MSI

--- Lab02_4.java
+++ Lab02_4.java
@@ -9,15 +9,31 @@ public class Lab02_4 {
         String secondCity = scanner.nextLine();
         System.out.print("Enter the third city: ");
         String thirdCity = scanner.nextLine();
-        if(firstCity.compareTo(secondCity)>0){
-            System.out.println("Hello");
+        String first = firstCity;
+        String second = secondCity;
+        String third = thirdCity;
+        if(firstCity.compareToIgnoreCase(secondCity)<=0 && firstCity.compareToIgnoreCase(thirdCity)<=0){
+            first = firstCity;
+            firstCity = secondCity;
+            secondCity = thirdCity;
         }
-        else if(firstCity.compareTo(secondCity)==0){
-            System.out.println("dfasdf");
+        else if(secondCity.compareToIgnoreCase(firstCity)<=0 && secondCity.compareToIgnoreCase(thirdCity)<=0){
+            first = secondCity;
+            secondCity = thirdCity; 
         }
-        else {
-            System.out.println("world");
+        else if(thirdCity.compareToIgnoreCase(firstCity)<=0 && thirdCity.compareToIgnoreCase(secondCity)<=0){
+            first = thirdCity;
         }
+        if(firstCity.compareToIgnoreCase(secondCity)<=0){
+            second = firstCity;
+            third = secondCity;
+        }
+        else{
+            second = secondCity;
+            third = firstCity;
+        }
+        System.out.println("The three cities in alphabetical order are " + first + " " + second + " " + third);
+        System.out.println(first + second + third);
     }
 }
 
