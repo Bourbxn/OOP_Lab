f19bf54587bfe196f2f5a04690fe9b3c1a7ee851
Jakkapat Paijit <86193685+Bourbxn@users.noreply.github.com>
2022-02-04 13:47:38 +0700

    MSI

--- Lab03_3.java
+++ Lab03_3.java
@@ -3,6 +3,27 @@ import java.util.Random;
 
 public class Lab03_3 {
     static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+        Random rand = new Random();
+        boolean inputError = false;
+        System.out.print("Enter the size for the matrix: ");
+        int num = scanner.nextInt();
+        if(num<2) inputError = true;
+        if(num<0) num*=-1;
+        int matrix[][] = new int[num][num];
+        for(int i=0;i<num && !inputError;i++){
+            for(int j=0;j<num;j++){
+                matrix[i][j]=rand.nextInt(2); 
+                System.out.print(matrix[i][j]);
+                if(j!=num-1) System.out.print(" ");
+            }
+            System.out.println();
+        }
+        if(inputError) System.out.println("INPUT ERROR!");
+        else checkMatrix(matrix, num);
+    }
+
     public static void checkSubdiagonal(int[][] matrix,int size){
         boolean haveSame = false;
         int same = 0;
@@ -93,25 +114,6 @@ public class Lab03_3 {
         checkSuperdiagonal(matrix, size);
         checkDiagonal(matrix, size);
         checkSubdiagonal(matrix, size);
-    }
-    
-    public static void main(String[] args){
-        Random rand = new Random();
-        boolean inputError = false;
-        System.out.print("Enter the size for the matrix: ");
-        int num = scanner.nextInt();
-        if(num<2) inputError = true;
-        int matrix[][] = new int[num][num];
-        for(int i=0;i<num;i++){
-            for(int j=0;j<num;j++){
-                matrix[i][j]=rand.nextInt(2);
-                System.out.print(matrix[i][j]);
-                if(j!=num-1) System.out.print(" ");
-            }
-            System.out.println();
-        }
-        if(inputError) System.out.println("INPUT ERROR!");
-        else checkMatrix(matrix, num);
     }
 }
 
--- Lab06_2/src/Course.java
+++
@@ -1,5 +0,0 @@
-class Course {
-    public Course(){
-        
-    }
-}
