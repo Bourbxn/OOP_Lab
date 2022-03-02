2570a8083d5122bc44d2e95ab349d74b20059d4d
Jakkapat Paijit <jakkapat97@gmail.com>
2022-03-02 20:23:00 +0700

    Macbook

--- Lab07_2/src/App.java
+++ Lab07_2/src/App.java
@@ -1,10 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Account account = new Account("George", "1122", 1000);
-        account.deposit(100);
-        account.withdraw(100000);
         account.deposit(30);
-        account.withdraw(20);
+        account.deposit(40);
+        account.deposit(50);
+        account.withdraw(5);
+        account.withdraw(4);
+        account.withdraw(2);
         account.showTransactions();
     }
 }
