0a66f2030d4a59a11472cb4f9d2a4cf186e89ba0
Jakkapat Paijit <jakkapat97@gmail.com>
2022-03-16 16:00:12 +0700

    Macbook

--- Lab08_2/src/App.java
+++ Lab08_2/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Square[] square = {
+            new Square(2),
+            new Square(3,"White",true),
+            new Square(4),
+            new Square(5,"Blue",true),
+            new Square(6)};   
+
+        for(int i=0;i<5;i++){
+            System.out.println("Square "+(i+1));
+            System.out.println(square[i]);
+            System.out.println();
+        }
     }
 }
