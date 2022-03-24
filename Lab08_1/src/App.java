842afdba557a89edf9e23d7a467d4cf570fd4367
Bourbxn <jakkapat97@gmail.com>
2022-03-24 21:00:20 +0700

    Macbook

--- Lab08_1/src/App.java
+++ Lab08_1/src/App.java
@@ -1,6 +1,22 @@
+import java.util.*;
+
 public class App {
+    static Scanner scanner = new Scanner(System.in);
+    static Scanner scannerBool = new Scanner(System.in);
+    static Scanner scannerStr = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        GeometricObject triangle = new Triangle(3,4,5,"Green",true); 
+        System.out.print("Enter side A: ");
+        double sideA = scanner.nextDouble();
+        System.out.print("Enter side B: ");
+        double sideB = scanner.nextDouble();
+        System.out.print("Enter side C: ");
+        double sideC = scanner.nextDouble();
+        System.out.print("Enter color: ");
+        String color = scannerStr.nextLine();
+        System.out.print("Enter filled: ");
+        boolean filled = scanner.nextBoolean();
+        System.out.println();
+        GeometricObject triangle = new Triangle(sideA,sideB,sideC,color,filled); 
         System.out.println(triangle);   
     }
 }
